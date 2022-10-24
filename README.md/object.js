a7ec6a5d8ca50ea644976062586a712e68638aa3
Civic86 <113317606+Civic86@users.noreply.github.com>
2022-10-24 11:23:02 -0700

    Add files via upload

---
+++ README.md/object.js
@@ -0,0 +1,12 @@
+var student = {
+    firstName: "Mark",
+    lastName: "Jacob",
+    age: 31,
+    employeeId: "A001234"
+}
+
+var sentence = "";
+
+sentence = "The new employee " + student.firstName + " " + student.lastName + " is " + student.age +  " years old. The employee ID is " + student.employeeId + ".";
+
+console.log(sentence);
